9705617d5781a8db150bcd03a608f376cb55d4c5
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-06-06 11:51:16 -0300

    exercicios de pratica

--- fundamentos/secao-08-introducao-a-higher-order-functions/dia-01-introducao-a-higher-order-functions/exercícios/ex003.js
+++
@@ -1,8 +0,0 @@
-const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
-
-const hasName = (arr, name) => {
-  return arr.some((elemento) => elemento === name);
-}
-
-console.log(hasName(names, 'Ana'));
-console.log(hasName(names, 'Pedro'));
