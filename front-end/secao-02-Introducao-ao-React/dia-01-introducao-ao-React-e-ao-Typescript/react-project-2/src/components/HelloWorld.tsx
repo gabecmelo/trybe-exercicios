2839c5444fb3104065b5c14b5b98c0bfaa693a5a
Gabriel Cabral Melo <contatogabemelo@gmail.com>
2023-07-03 12:17:06 -0300

    Prática

---
+++ front-end/secao-02-Introducao-ao-React/dia-01-introducao-ao-React-e-ao-Typescript/react-project-2/src/components/HelloWorld.tsx
@@ -0,0 +1,18 @@
+import Title from './Title';
+import ModuleDetails from './ModuleDetails';
+import { calculateAge } from '../utils/age';
+
+export default function HelloWorld() {
+  const name = 'Gabriel Melo';
+  const birth = '12/01/2005';
+  const age = calculateAge(birth);
+
+  return (
+    <div>
+      <h1 style={ { color: 'green', textAlign: 'center' } }>{name}</h1>
+      <p>{`${age} anos`}</p>
+      <Title />
+      <ModuleDetails />
+    </div>
+  );
+}
---
+++ front-end/secao-02-Introducao-ao-React/dia-01-introducao-ao-React-e-ao-Typescript/react-project-2/src/components/ModuleDetails.tsx
@@ -0,0 +1,10 @@
+export default function ModuleDetails() {
+  return (
+    <ul>
+      <li>Módulo: Fundamentos ✅</li>
+      <li>Módulo: Front-end ⏳</li>
+      <li>Módulo: Back-end ⏳</li>
+      <li>Módulo: Ciência da Computação ⏳</li>
+    </ul>
+  );
+}
